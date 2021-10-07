7b5622bb8370f0be04b0d6692bd7e277bd377c73
Mansi Varshney <70686638+mansivar21-22@users.noreply.github.com>
2021-10-07 10:40:05 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,29 @@
+
+
+//function calc(func,a,b){
+  //  console.log("calc invoked");
+ //return func(a,b);
+//}
+/*function sum(a,b){
+    return a+b;
+}
+
+const result = calculator(sumValue,23,45);
+console.log(result);
+
+//const result = calculator(function(a,b){
+  //  return a+b;
+//},23,45);  
+console.log(result);
+
+console.log(calc(sum,10,20));
+
+console.log(calc(function(a,b){
+    return a,b;
+},10,5));*/
+
+//const result = calc((a,b)=> a+b ,20,20);
+//console.log(result);
+
+import { sayHi } from "./second";
+sayHi("Zoya");
